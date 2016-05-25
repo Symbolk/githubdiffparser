6b48f119326eb4429a614ecf5dcff0ce62714230
Stuart Kent <skent.dev@gmail.com>
2016-05-25 10:07:53 -0400

    Refactor hunk start pattern into constant

--- diffparser/src/main/java/org/wickedsource/diffparser/api/UnifiedDiffParser.java
+++ diffparser/src/main/java/org/wickedsource/diffparser/api/UnifiedDiffParser.java
@@ -28,8 +28,10 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class UnifiedDiffParser implements DiffParser {
+import static org.wickedsource.diffparser.unified.Constants.HUNK_START_PATTERN;
 
+public class UnifiedDiffParser implements DiffParser {
+    
     @Override
     public List<Diff> parse(InputStream in) {
         ResizingParseWindow window = new ResizingParseWindow(in);
@@ -92,8 +94,8 @@ public class UnifiedDiffParser implements DiffParser {
     }
 
     private void parseHunkStart(Diff currentDiff, String currentLine) {
-        Pattern pattern = Pattern.compile("^.*-([0-9]+)(?:,([0-9]+))? \\+([0-9]+)(?:,([0-9]+))?.*$");
-        Matcher matcher = pattern.matcher(currentLine);
+        Matcher matcher = HUNK_START_PATTERN.matcher(currentLine);
+        
         if (matcher.matches()) {
             String range1Start = matcher.group(1);
             String range1Count = (matcher.group(2) != null) ? matcher.group(2) : "1";
